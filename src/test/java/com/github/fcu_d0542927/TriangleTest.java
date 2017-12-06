21b3dc3418fa046cd06ba86f97482ceefa1f8294
fcu-d0542927 <ikenken555@yahoo.com.tw>
2017-12-06 12:04:30 +0800

    V1.0

---
+++ src/test/java/com/github/fcu_d0542927/TriangleTest.java
@@ -0,0 +1,18 @@
+package com.github.fcu_d0542927;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+    @Test
+    public void getType() throws Exception {
+        @
+
+
+    }
+
+
+
+
+}
---
+++ src/test/java/com/github/fcu_d0542927/testTest.java
@@ -0,0 +1,13 @@
+package com.github.fcu_d0542927;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class testTest {
+    @Test
+    public void getType() throws Exception {
+
+    }
+
+}
