a0e85c9cccf4885d083171e08041786ec4fc453b
fcu-d0542927 <ikenken555@yahoo.com.tw>
2017-12-06 12:35:52 +0800

    V2.0

--- src/test/java/com/github/fcu_d0542927/TriangleTest.java
+++ src/test/java/com/github/fcu_d0542927/TriangleTest.java
@@ -1,5 +1,6 @@
 package com.github.fcu_d0542927;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,7 +8,12 @@ import static org.junit.Assert.*;
 public class TriangleTest {
     @Test
     public void getType() throws Exception {
-        @
+
+        Triangle triangle = new Triangle( 5 ,  10 ,  5);
+
+        String result = triangle.getType();
+
+        Assert.assertEquals("不是三角形" ,result);
 
 
     }
